269aca593ff5568228ae169bff0359bedeb264ae
Mahmoud Yasser <mle.mahmoud.yasser@gmail.com>
2021-04-24 10:50:22 +0200

    :construction_worker: Add Token Services

---
+++ src/services/token.service.js
@@ -0,0 +1,166 @@
+import jwt from 'jsonwebtoken';
+import moment from 'moment';
+import config from '../config/config';
+import { getUserByEmail } from './user.service';
+import { Token } from '../models/index';
+import AppError from '../utils/appError';
+import tokenTypes from '../config/tokens';
+
+/**
+ * Generate token
+ * @param {ObjectId} userId
+ * @param {Moment} expires
+ * @param {string} [secret]
+ * @returns {string}
+ */
+export const generateToken = (
+  userId,
+  expires,
+  type,
+  secret = config.jwt.secret
+) => {
+  const payload = {
+    sub: userId,
+    iat: moment().unix(),
+    exp: expires.unix(),
+    type
+  };
+  return jwt.sign(payload, secret);
+};
+
+/**
+ * Save a token
+ * @param {string} token
+ * @param {ObjectId} userId
+ * @param {Moment} expires
+ * @param {string} type
+ * @param {boolean} [blacklisted]
+ * @returns {Promise<Token>}
+ */
+export const saveToken = async (
+  token,
+  userId,
+  expires,
+  type,
+  blacklisted = false
+) => {
+  const tokenDoc = await Token.create({
+    token,
+    user: userId,
+    expires: expires.toDate(),
+    type,
+    blacklisted
+  });
+  return tokenDoc;
+};
+
+/**
+ * Verify token and return token doc (or throw an error if it is not valid)
+ * @param {string} token
+ * @param {string} type
+ * @returns {Promise<Token>}
+ */
+export const verifyToken = async (token, type) => {
+  const payload = jwt.verify(token, config.jwt.secret);
+  const tokenDoc = await Token.findOne({
+    token,
+    type,
+    user: payload.sub,
+    blacklisted: false
+  });
+  if (!tokenDoc) {
+    throw new Error('Token not found');
+  }
+  return tokenDoc;
+};
+
+/**
+ * Generate auth tokens
+ * @param {User} user
+ * @returns {Promise<Object>}
+ */
+export const generateAuthTokens = async (user) => {
+  const accessTokenExpires = moment().add(
+    config.jwt.accessExpirationMinutes,
+    'minutes'
+  );
+  const accessToken = generateToken(
+    user.id,
+    accessTokenExpires,
+    tokenTypes.ACCESS
+  );
+
+  const refreshTokenExpires = moment().add(
+    config.jwt.refreshExpirationDays,
+    'days'
+  );
+  const refreshToken = generateToken(
+    user.id,
+    refreshTokenExpires,
+    tokenTypes.REFRESH
+  );
+  await saveToken(
+    refreshToken,
+    user.id,
+    refreshTokenExpires,
+    tokenTypes.REFRESH
+  );
+
+  return {
+    access: {
+      token: accessToken,
+      expires: accessTokenExpires.toDate()
+    },
+    refresh: {
+      token: refreshToken,
+      expires: refreshTokenExpires.toDate()
+    }
+  };
+};
+
+/**
+ * Generate reset password token
+ * @param {string} email
+ * @returns {Promise<string>}
+ */
+export const generateResetPasswordToken = async (email) => {
+  const user = await getUserByEmail(email);
+  if (!user) {
+    throw new AppError('No users found with this email', 404);
+  }
+  const expires = moment().add(
+    config.jwt.resetPasswordExpirationMinutes,
+    'minutes'
+  );
+  const resetPasswordToken = generateToken(
+    user.id,
+    expires,
+    tokenTypes.RESET_PASSWORD
+  );
+  await saveToken(
+    resetPasswordToken,
+    user.id,
+    expires,
+    tokenTypes.RESET_PASSWORD
+  );
+  return resetPasswordToken;
+};
+
+/**
+ * Generate verify email token
+ * @param {string} email
+ * @returns {Promise<string>}
+ */
+export const generateVerifyEmailToken = async (user) => {
+  const expires = moment().add(
+    config.jwt.verifyEmailExpirationMinutes,
+    'minutes'
+  );
+  const verifyEmailToken = generateToken(
+    user.id,
+    expires,
+    tokenTypes.VERIFY_EMAIL
+  );
+  await saveToken(verifyEmailToken, user.id, expires, tokenTypes.VERIFY_EMAIL);
+  return verifyEmailToken;
+};
