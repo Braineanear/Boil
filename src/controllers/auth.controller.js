383d1aa398649ceb48e63f41ca98dbf79e013d80
Mahmoud Yasser <mle.mahmoud.yasser@gmail.com>
2021-04-24 10:56:09 +0200

    :sparkles: Add Auth Controller

---
+++ src/controllers/auth.controller.js
@@ -0,0 +1,56 @@
+import catchAsync from '../utils/catchAsync';
+import {
+  authService,
+  userService,
+  tokenService,
+  emailService
+} from '../services/index';
+
+export const register = catchAsync(async (req, res) => {
+  const user = await userService.createUser(req.body);
+  const tokens = await tokenService.generateAuthTokens(user);
+  res.status(201).send({ user, tokens });
+});
+
+export const login = catchAsync(async (req, res) => {
+  const { email, password } = req.body;
+  const user = await authService.loginUserWithEmailAndPassword(email, password);
+  const tokens = await tokenService.generateAuthTokens(user);
+  res.send({ user, tokens });
+});
+
+export const logout = catchAsync(async (req, res) => {
+  await authService.logout(req.body.refreshToken);
+  res.status(204).send();
+});
+
+export const refreshTokens = catchAsync(async (req, res) => {
+  const tokens = await authService.refreshAuth(req.body.refreshToken);
+  res.send({ ...tokens });
+});
+
+export const forgotPassword = catchAsync(async (req, res) => {
+  const resetPasswordToken = await tokenService.generateResetPasswordToken(
+    req.body.email
+  );
+  await emailService.sendResetPasswordEmail(req.body.email, resetPasswordToken);
+  res.status(204).send();
+});
+
+export const resetPassword = catchAsync(async (req, res) => {
+  await authService.resetPassword(req.query.token, req.body.password);
+  res.status(204).send();
+});
+
+export const sendVerificationEmail = catchAsync(async (req, res) => {
+  const verifyEmailToken = await tokenService.generateVerifyEmailToken(
+    req.user
+  );
+  await emailService.sendVerificationEmail(req.user.email, verifyEmailToken);
+  res.status(204).send();
+});
+
+export const verifyEmail = catchAsync(async (req, res) => {
+  await authService.verifyEmail(req.query.token);
+  res.status(204).send();
+});
